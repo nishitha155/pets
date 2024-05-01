b8ca4938349c5cb4d295e55599c78ab65fac86ea
nishitha155 <122442970+nishitha155@users.noreply.github.com>
2024-05-01 22:40:16 +0530

    Update UserLogin.jsx

--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -50,7 +50,7 @@ const UserLogin = () => {
     }
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3002/api/user/login', {
+      const response = await fetch('https://pets-2.onrender.com/api/user/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
