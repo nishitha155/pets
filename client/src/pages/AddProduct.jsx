f5316f1f0e4acd4ece5ecf19a9b9d3fcfeae28a0
nishitha155 <122442970+nishitha155@users.noreply.github.com>
2024-05-01 22:40:32 +0530

    Update AddProduct.jsx

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -115,7 +115,7 @@ const Addproduct = () => {
       formData.append('price', priceValue);
       formData.append('quantity', quantityValue);
       formData.append('image', file);
-      const response = await fetch('http://localhost:3002/productupload', {
+      const response = await fetch('https://pets-2.onrender.com/productupload', {
         method: 'POST',
         body: formData,
         encType:'multipart/form'
@@ -150,7 +150,7 @@ const Addproduct = () => {
     formData.append('file', csvfile);
 
     try {
-      const response = await axios.post('http://localhost:3002/csvupload', formData, {
+      const response = await axios.post('https://pets-2.onrender.com/csvupload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
