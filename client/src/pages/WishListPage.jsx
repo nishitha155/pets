2e36659d15472048531fb1dc3563704b6ffcaac3
nishitha155 <122442970+nishitha155@users.noreply.github.com>
2024-05-01 22:37:55 +0530

    Update WishListPage.jsx

--- client/src/pages/WishListPage.jsx
+++ client/src/pages/WishListPage.jsx
@@ -13,7 +13,7 @@ const WishlistPage = () => {
   useEffect(() => {
     const fetchWishlist = async () => {
       try {
-        const response = await fetch(`http://localhost:3002/api/wishlist/${userid}`);
+        const response = await fetch(`https://pets-2.onrender.com/api/wishlist/${userid}`);
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
         }
