a89fce39e9c2a29ecb1781c51a0f3e0e703e1070
nishitha155 <122442970+nishitha155@users.noreply.github.com>
2024-05-01 22:54:09 +0530

    Update SaloonSingle.jsx

--- client/src/pages/SaloonSingle.jsx
+++ client/src/pages/SaloonSingle.jsx
@@ -14,7 +14,7 @@ const SaloonSingle = () => {
     const fetchSalonDetails = async () => {
       try {
         console.log('hi')
-        const response = await fetch(`http://localhost:3002/salons/${title}`);
+        const response = await fetch(`https://pets-2.onrender.com/salons/${title}`);
 
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
