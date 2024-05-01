e9db0f92f600be08e40584931eb72350753e2a29
nishitha155 <122442970+nishitha155@users.noreply.github.com>
2024-05-01 22:49:05 +0530

    Update AdminOrders.jsx

--- client/src/pages/AdminOrders.jsx
+++ client/src/pages/AdminOrders.jsx
@@ -14,7 +14,7 @@ const AdminOrders = () => {
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const response = await fetch('http://localhost:3002/api/orders');
+        const response = await fetch('https://pets-2.onrender.com/api/orders');
         const data = await response.json();
         setOrders(data);
       } catch (error) {
@@ -90,7 +90,7 @@ const AdminOrders = () => {
                         {order.products.map(product => (
                           <>
                             <div key={product.title} style={{ display: 'flex',flexWrap:'wrap' }}>
-                              <Image src={`http://localhost:3002/uploads/${product.image}`} alt={product.title} boxSize="5vw" style={{ margin: '1px 58px' }} />
+                              <Image src={`https://pets-2.onrender.com/uploads/${product.image}`} alt={product.title} boxSize="5vw" style={{ margin: '1px 58px' }} />
                               <Box style={{ display: 'flex',flexWrap:'wrap' }} >
                                 <Box style={{ width: '27rem' }}>
                                   <p style={{
